8f821e9d6d3f2bd585acfb36f486a6198dd5293a
Rivea108 <zkdhtm152@naver.com>
2025-02-12 19:42:52 +0900

    short_url_practice_02 END

---
+++ src/main/java/com/koreait/short_url_practice_02/ShortUrlPractice02Application.java
@@ -0,0 +1,13 @@
+package com.koreait.short_url_practice_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlPractice02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlPractice02Application.class, args);
+    }
+
+}
