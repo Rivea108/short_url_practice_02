756d8fca2634242af20a183cb7f85606c96f86a4
woooyoung <axdsw121@gmail.com>
2025-02-14 14:42:08 +0900

    여러가지 SELECT

--- src/main/java/com/koreait/short_url_project_02/global/initData/NotProd.java
+++ src/main/java/com/koreait/short_url_project_02/global/initData/NotProd.java
@@ -4,44 +4,77 @@ import com.koreait.short_url_project_02.domain.article.article.entity.Article;
 import com.koreait.short_url_project_02.domain.article.article.repository.ArticleRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
+import java.util.List;
+import java.util.Optional;
+
 //!prod == dev or test
+
 @Profile("!prod")
 @Configuration
 @RequiredArgsConstructor
 public class NotProd {
+    // this를 통한 객체 내부에서의 메서드 호출은 @Transactional을 작동시키지 않아
+    // 외부객체에 의한 메서드 호출은 @Transactional이 작동해
+    // @Lazy, @Autowired 조합은 this의 외부 호출 모드 버전의 self 를 얻을 수 있어
+    // self를 통한 메서드 호출은 @Transactional을 작동시킬 수 있어
+
+    @Lazy
+    @Autowired
+    private NotProd self;
 
     private final ArticleRepository articleRepository;
 
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
     public ApplicationRunner initDataProd() {
-        return new ApplicationRunner() {
-            @Override
-            @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-
-                if (articleRepository.count() > 0) return;
-
-                Article article1 = Article.builder().
-                        title("제목1")
-                        .body("내용1").build();
-                Article article2 = Article.builder().
-                        title("제목2")
-                        .body("내용2").build();
-
-                articleRepository.save(article1);
-                articleRepository.save(article2);
-
-                article2.setTitle("제목 2-2");
-
-                articleRepository.delete(article1);
-
-            }
+        return args -> {
+            self.work1();
+            self.work2();
         };
     }
+
+    @Transactional
+    public void work1() {
+        if (articleRepository.count() > 0) return;
+
+        Article article1 = Article.builder().
+                title("제목1")
+                .body("내용1").build();
+        Article article2 = Article.builder().
+                title("제목2")
+                .body("내용2").build();
+
+        articleRepository.save(article1);
+        articleRepository.save(article2);
+
+        article2.setTitle("제목 2-2");
+
+        articleRepository.delete(article1);
+
+    }
+
+    @Transactional
+    public void work2() {
+        // List : 0 ~ N
+        // Optional : 0 ~ 1
+        Optional<Article> opArticle = articleRepository.findById(2L); // jpa 기본제공
+        List<Article> articles = articleRepository.findAll(); // jpa 기본제공
+
+        List<Article> articlesByIn = articleRepository.findByIdInOrderByTitleDescIdAsc(List.of(1L, 2L));
+
+        System.out.println(articlesByIn.get(0).getTitle());
+
+        List<Article> articlesByLIKE = articleRepository.findByTitleContaining("제목");
+
+        articleRepository.findByTitleAndBody("제목", "내용");
+
+    }
 }
+
+
